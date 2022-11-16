1d09ae338dc0dda8b1c939946b97cdebbbdffd11
Isaad Khan <IKhan@lcs.on.ca>
2022-11-16 12:08:33 -0500

    Added a tab bar to preview window for ContentView.

--- StopwatchApp/StopwatchAppApp.swift
+++ StopwatchApp/StopwatchAppApp.swift
@@ -11,7 +11,32 @@ import SwiftUI
 struct StopwatchAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            TabView{
+                
+                Text("World Clock")
+                    .tabItem {
+                        Image(systemName: "globe")
+                        Text("World Clock")
+                    }
+                Text("Alarm")
+                    .tabItem {
+                        Image(systemName: "alarm.fill")
+                        Text("Alarm")
+                    }
+                
+                ContentView()
+                    .tabItem {
+                        Image(systemName: "stopwatch.fill")
+                        Text("Stopwatch")
+                    }
+                
+                Text("Timer")
+                    .tabItem {
+                        Image(systemName: "timer")
+                        Text("Timer")
+                    }
+            }
+            
         }
     }
 }
